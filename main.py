a81c1f65b971034b12fa2b7c80414b6f4cb122d6
suhail_shamim <suhailshamim10@gmail.com>
2022-11-23 17:23:12 +0530

    Downloaded the data and extracted it for analysis.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("This is my first git repository commit for the new")
+# Script to Extract data from the zip file.
+
+from zipfile import ZipFile
+import os
+
+
+path = '~/projects/ml_ai_projects/git_first_pull/Data.zip'
+
+file_name = str(os.path.basename(path))
+
+
+with ZipFile(file_name) as zip:
+    zip.extractall()
+
+  
